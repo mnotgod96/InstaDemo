126221fd1a8e86100d3f41bd3e9b8cbded94d78f
Jiaxuan Liu <jliu158@illinois.edu>
2020-01-17 01:22:08 -0500

    add CRUD to the web

--- Insta/urls.py
+++ Insta/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from Insta.views import HelloWorld
+from Insta.views import HelloWorld, PostsView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-  path('', HelloWorld.as_view(), name='helloworld')
+  path('', HelloWorld.as_view(), name='helloworld'),
+  path('posts/', PostsView.as_view(), name='posts'),
+  path('posts/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+  path('posts/new/', PostCreateView.as_view(), name='make_post'),
+  path('posts/update/<int:pk>/', PostUpdateView.as_view(), name='update_post'),
+  path('posts/delete/<int:pk>/', PostDeleteView.as_view(), name='delete_post')
 ]
--- Insta/views.py
+++ Insta/views.py
@@ -1,7 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from Insta.models import Post
 
 class HelloWorld(TemplateView):
   template_name = 'test.html'
+
+class PostsView(ListView):
+  model = Post
+  template_name = 'index.html'
+
+class PostDetailView(DetailView):
+  model = Post
+  template_name = 'post_detail.html'
+
+class PostCreateView(CreateView):
+  model = Post
+  template_name = 'make_post.html'
+  fields = '__all__'
+
+class PostUpdateView(UpdateView):
+  model = Post
+  template_name = 'update_post.html'
+  fields = ['title']
+
+class PostDeleteView(DeleteView):
+  model = Post
+  template_name = 'delete_post.html'
+  success_url = reverse_lazy("posts")
