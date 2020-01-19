4f11ec74372893c126e6c224865e2906585a5f9b
Jiaxuan Liu <jliu158@illinois.edu>
2020-01-19 05:03:16 -0500

    add authentication and custom user model

--- Insta/views.py
+++ Insta/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from Insta.forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from Insta.models import Post
 
@@ -18,10 +20,11 @@ class PostDetailView(DetailView):
   model = Post
   template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
   model = Post
   template_name = 'make_post.html'
   fields = '__all__'
+  login_url = 'login'
 
 class PostUpdateView(UpdateView):
   model = Post
@@ -32,3 +35,8 @@ class PostDeleteView(DeleteView):
   model = Post
   template_name = 'delete_post.html'
   success_url = reverse_lazy("posts")
+
+class SignUp(CreateView):
+  form_class = CustomUserCreationForm
+  template_name = 'signup.html'
+  success_url = reverse_lazy("login")
