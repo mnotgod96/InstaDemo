5831ed8265cd485c253efb630588e41aaabdbdfd
jliu158 <jliu158@illinois.edu>
2020-02-02 06:30:10 -0600

    ui update, add like feature

--- Insta/models.py
+++ Insta/models.py
@@ -4,7 +4,17 @@ from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class InstaUser(AbstractUser):
+  profile_pic = ProcessedImageField(
+    upload_to='static/images/posts',
+    format='JPEG',
+    options={'quality':100},
+    blank=True,
+    null=True
+  )
+
 class Post(models.Model):
+  author = models.ForeignKey(InstaUser, on_delete=models.CASCADE, related_name='my_posts')
   title = models.TextField(blank=True, null=True)
   # image = models.ImageField
   image = ProcessedImageField(
@@ -17,12 +27,16 @@ class Post(models.Model):
 
   def get_absolute_url(self):
     return reverse("post_detail", args=[str(self.id)])
+  
+  def get_like_count(self):
+    return self.likes.count()
 
-class InstaUser(AbstractUser):
-  profile_pic = ProcessedImageField(
-    upload_to='static/images/posts',
-    format='JPEG',
-    options={'quality':100},
-    blank=True,
-    null=True
-  )
+class Like(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes')
+  user = models.ForeignKey(InstaUser, on_delete=models.CASCADE, related_name='likes')
+
+  class Meta:
+    unique_together = ("post", "user")
+  
+  def __str__(self):
+    return 'Like: ' + self.user.username + ' likes ' + self.post.title
